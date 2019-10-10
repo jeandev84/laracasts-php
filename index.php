866e6663a926a39a8b65e1df38d8319be86edb8d
Yao Jean-Claude <jeandev84@yahoo.com>
2019-10-10 19:24:09 +0300

    Variables

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
 
 /**
- * Class Report
+ * Variables
  */
-class Report
-{
-    /**
-     * @param $user
-     */
-     public function generate($user)
-     {
-         // TODO Implements
-     }
-}
 
-echo "Hello World! \n";
+/*
+$greeting = 'Hello Universe!';
+echo $greeting . "\n";
+
+$name = 'Jean-Claude';
+echo "Hello, $name \n";
+*/
+
+$greeting = 'Hello';
+$name = 'John Brown';
+
+//echo $greeting . ', '. $name ."\n";
+echo "{$greeting}, {$name}";
