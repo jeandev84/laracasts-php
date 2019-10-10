6c3eff77a46b17a80e2d8a761af4cd3f896c4a88
Yao Jean-Claude <jeandev84@yahoo.com>
2019-10-10 20:07:35 +0300

    PHP and HTML

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <?php
 
 /**
- * Variables
+ * @url http://localhost:8888?order=asc&keys=values
+ * @url http://localhost:8888?order=desc
+ * @url http://localhost:8888?name=John ( Mary, Jeff )
  */
 
-/*
-$greeting = 'Hello Universe!';
-echo $greeting . "\n";
-
-$name = 'Jean-Claude';
-echo "Hello, $name \n";
-*/
-
-$greeting = 'Hello';
-$name = 'John Brown';
-
-//echo $greeting . ', '. $name ."\n";
-echo "{$greeting}, {$name}";
+$name = $_GET['name'] ?? null;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="/assets/css/app.css">
+</head>
+<body>
+   <header>
+       <h1><?= "Hello, ". htmlspecialchars($name) ?></h1>
+   </header>
+</body>
+</html>
