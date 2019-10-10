7721b69651749e90b639923cd4acfc44da47a054
Yao Jean-Claude <jeandev84@yahoo.com>
2019-10-11 00:55:50 +0300

    Conditionals

---
+++ coding/conditions/cond2/index.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ - title,
+ - due,
+ - assigned_to ,
+ - completed (boolean: true or false / yes or no)
+   echo (completed == 'yes' ? 'is completed' : 'no completed');
+   if($completed) {} or if(! $completed) {}
+
+*/
+
+$task = [
+    'title' => 'Finish homework',
+    'due' => 'today',
+    'assigned_to' => 'Jeffrey',
+    'completed' => true, // false
+];
+
+
+require 'index.view.php';
+
+
+
