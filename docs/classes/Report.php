88b0e8f0c40442d7a52aa3cf8a77fb873c79e287
Yao Jean-Claude <jeandev84@yahoo.com>
2019-10-10 18:35:40 +0300

    Intro php

---
+++ docs/classes/Report.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Class Report
+ */
+class Report
+{
+    /**
+     * @param $user
+     */
+    public function generate($user)
+    {
+        // TODO Implements
+    }
+}
+
+echo "Hello World! \n";
